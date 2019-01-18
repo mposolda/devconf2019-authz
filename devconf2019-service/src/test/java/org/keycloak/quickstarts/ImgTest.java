bf5d3b69e2cc1bdb833fa7b82c0476c684909ba1
mposolda <mposolda@gmail.com>
2019-01-18 10:50:35 +0100

    Added modules

---
+++ devconf2019-service/src/test/java/org/keycloak/quickstarts/ImgTest.java
@@ -0,0 +1,11 @@
+package org.keycloak.quickstarts;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class ImgTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
